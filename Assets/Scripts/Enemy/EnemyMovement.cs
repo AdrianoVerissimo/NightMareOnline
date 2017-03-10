14a8939b99901729d7457c601b12492de3bba408
Adriano Veríssimo <adrverissimo@yahoo.com.br>
2017-03-09 21:06:10 -0300

    Add inimigo e fazendo-o seguir o jogador.

--- Assets/Scripts/Enemy/EnemyMovement.cs
+++ Assets/Scripts/Enemy/EnemyMovement.cs
@@ -8,13 +8,13 @@ public class EnemyMovement : MonoBehaviour
     //EnemyHealth enemyHealth;
     UnityEngine.AI.NavMeshAgent nav;
 
-
+	//executado ao iniciar, mesmo o script estando inativado
     void Awake ()
     {
         player = GameObject.FindGameObjectWithTag ("Player").transform;
         //playerHealth = player.GetComponent <PlayerHealth> ();
         //enemyHealth = GetComponent <EnemyHealth> ();
-        nav = GetComponent <UnityEngine.AI.NavMeshAgent> ();
+        nav = GetComponent <UnityEngine.AI.NavMeshAgent> (); //pega o agente a ser usado para fazer a perseguição
     }
 
 
@@ -22,7 +22,7 @@ public class EnemyMovement : MonoBehaviour
     {
         //if(enemyHealth.currentHealth > 0 && playerHealth.currentHealth > 0)
         //{
-            nav.SetDestination (player.position);
+            nav.SetDestination (player.position); //faz o inimigo seguir a posição do jogador
         //}
         //else
         //{
