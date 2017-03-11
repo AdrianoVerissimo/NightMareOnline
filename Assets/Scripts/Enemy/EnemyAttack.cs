0a58aea12d81b2912f18987401b89182c38e14a9
Adriano Veríssimo <adrverissimo@yahoo.com.br>
2017-03-10 22:43:06 -0300

    Fazendo o jogador levar dano ao encostar no inimigo.

--- Assets/Scripts/Enemy/EnemyAttack.cs
+++ Assets/Scripts/Enemy/EnemyAttack.cs
@@ -3,68 +3,73 @@ using System.Collections;
 
 public class EnemyAttack : MonoBehaviour
 {
-    public float timeBetweenAttacks = 0.5f;
-    public int attackDamage = 10;
+    public float timeBetweenAttacks = 0.5f; //tempo para efetuar próximo ataque, após atacar
+    public int attackDamage = 10; //quantidade de dano
 
 
-    Animator anim;
-    GameObject player;
-    PlayerHealth playerHealth;
+    Animator anim; //referência para o Animator
+    GameObject player; //referência para o jogador
+    PlayerHealth playerHealth; //referência para o script de energia do jogador
     //EnemyHealth enemyHealth;
-    bool playerInRange;
+    bool playerInRange; //indica se o jogador está ao alcance
     float timer;
 
 
     void Awake ()
     {
-        player = GameObject.FindGameObjectWithTag ("Player");
-        playerHealth = player.GetComponent <PlayerHealth> ();
+		player = GameObject.FindGameObjectWithTag ("Player"); //resgata referência para o objeto do jogador
+        playerHealth = player.GetComponent <PlayerHealth> (); //pega referência para o script de energia do jogador
         //enemyHealth = GetComponent<EnemyHealth>();
-        anim = GetComponent <Animator> ();
+        anim = GetComponent <Animator> (); //referência para o componente Animator
     }
 
-
+	//aciona este trigger quando algo colide
     void OnTriggerEnter (Collider other)
     {
+		//se o outro objeto é o jogador
         if(other.gameObject == player)
         {
-            playerInRange = true;
+            playerInRange = true; //define que o jogador está ao alcance para um ataque
         }
     }
 
-
+	//aciona este trigger quando algo sai da colisão
     void OnTriggerExit (Collider other)
     {
         if(other.gameObject == player)
         {
-            playerInRange = false;
+            playerInRange = false; //define que o jogador não está mais ao alcance para um ataque
         }
     }
 
 
     void Update ()
     {
+		//incrementa o tempo
         timer += Time.deltaTime;
 
+		//o tempo que passou é maior ou igual ao tempo para atacar e o jogador está ao alcance
         if(timer >= timeBetweenAttacks && playerInRange/* && enemyHealth.currentHealth > 0*/)
         {
-            Attack ();
+            Attack (); //atacar
         }
 
+		//energia do jogador é 0 ou menor
         if(playerHealth.currentHealth <= 0)
         {
-            anim.SetTrigger ("PlayerDead");
+            anim.SetTrigger ("PlayerDead"); //aciona animação de que o jogador morreu
         }
     }
 
-
+	//método para atacar
     void Attack ()
     {
-        timer = 0f;
+        timer = 0f; //reseta o tempo passado
 
+		//o jogador está vivo
         if(playerHealth.currentHealth > 0)
         {
-            playerHealth.TakeDamage (attackDamage);
+            playerHealth.TakeDamage (attackDamage); //faz o jogador levar dano
         }
     }
 }
--- Assets/Scripts/Player/PlayerHealth.cs
+++ Assets/Scripts/Player/PlayerHealth.cs
@@ -1,33 +1,33 @@
 ﻿using UnityEngine;
-using UnityEngine.UI;
+using UnityEngine.UI; //necessário para utilizar componentes da UI
 using System.Collections;
 using UnityEngine.SceneManagement;
 
 
 public class PlayerHealth : MonoBehaviour
 {
-    public int startingHealth = 100;
-    public int currentHealth;
-    public Slider healthSlider;
-    public Image damageImage;
-    public AudioClip deathClip;
-    public float flashSpeed = 5f;
-    public Color flashColour = new Color(1f, 0f, 0f, 0.1f);
+    public int startingHealth = 100; //energia inicial
+    public int currentHealth; //energia atual
+    public Slider healthSlider; //referência para o slider da energia
+    public Image damageImage; //referência para imagem mostrada ao levar dano
+    public AudioClip deathClip; //audio tocado quando o jogador morre
+    public float flashSpeed = 5f; //velocidade em que a imagem de dano aparecerá
+    public Color flashColour = new Color(1f, 0f, 0f, 0.1f); //cor da imagem de dano
 
 
-    Animator anim;
-    AudioSource playerAudio;
-    PlayerMovement playerMovement;
+    Animator anim; //referência para componente de animação
+    AudioSource playerAudio; //referência para o componente Audio Source
+    PlayerMovement playerMovement; //referência para o script playerMovement
     //PlayerShooting playerShooting;
-    bool isDead;
-    bool damaged;
+    bool isDead; //diz se o jogador está morto
+    bool damaged; //diz se o jogadro levou dano
 
 
     void Awake ()
     {
         anim = GetComponent <Animator> ();
         playerAudio = GetComponent <AudioSource> ();
-        playerMovement = GetComponent <PlayerMovement> ();
+        playerMovement = GetComponent <PlayerMovement> (); //pega referência ao script atrelado ao objeto do jogador como componente
         //playerShooting = GetComponentInChildren <PlayerShooting> ();
         currentHealth = startingHealth;
     }
@@ -35,47 +35,52 @@ public class PlayerHealth : MonoBehaviour
 
     void Update ()
     {
+		//levou dano
         if(damaged)
         {
+			//coloca uma cor na imagem de dano
             damageImage.color = flashColour;
         }
         else
         {
+			//faz um fading para a imagem de dano sumir
             damageImage.color = Color.Lerp (damageImage.color, Color.clear, flashSpeed * Time.deltaTime);
         }
-        damaged = false;
+        damaged = false; //define como falso para já começar no próximo frame o efeito de fading na imagem de dano
     }
 
-
+	//método utilizado para fazer o jogador levar o dano passado por parâmetro
     public void TakeDamage (int amount)
     {
-        damaged = true;
+        damaged = true; //define que levou dano
 
-        currentHealth -= amount;
+        currentHealth -= amount; //subtrai o dano da energia atual
 
-        healthSlider.value = currentHealth;
+        healthSlider.value = currentHealth; //atualiza o slider da energia
 
-        playerAudio.Play ();
+        playerAudio.Play (); //toca o áudio de dano
 
+		//se a energia for 0 ou menor, o jogador morreu
         if(currentHealth <= 0 && !isDead)
         {
             Death ();
         }
     }
 
-
+	//define se o jogador morreu
     void Death ()
     {
         isDead = true;
 
         //playerShooting.DisableEffects ();
 
-        anim.SetTrigger ("Die");
+        anim.SetTrigger ("Die"); //aciona o trigger Die definido no animator, para rodar a animação que morreu
 
+		//áudio de que o jogador morreu
         playerAudio.clip = deathClip;
         playerAudio.Play ();
 
-        playerMovement.enabled = false;
+        playerMovement.enabled = false; //desabilita a movimentação do jogador
         //playerShooting.enabled = false;
     }
 
