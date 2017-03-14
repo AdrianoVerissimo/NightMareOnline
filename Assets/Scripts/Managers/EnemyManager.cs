3ab50a4cad99426d010667090d60bbdb4db3514d
Adriano Veríssimo <adrverissimo@yahoo.com.br>
2017-03-14 17:24:18 -0300

    Criando prefab para 2 novos inimigos; fazendo os inimigos aparecerem de tempo em tempo.

--- Assets/Scripts/Managers/EnemyManager.cs
+++ Assets/Scripts/Managers/EnemyManager.cs
@@ -2,27 +2,29 @@
 
 public class EnemyManager : MonoBehaviour
 {
-    public PlayerHealth playerHealth;
-    public GameObject enemy;
-    public float spawnTime = 3f;
-    public Transform[] spawnPoints;
+    public PlayerHealth playerHealth; //referência para energia do jogador
+    public GameObject enemy; //referência para o inimigo que queremos dar spawn
+    public float spawnTime = 3f; //tempo para dar spawn em um novo inimigo
+    public Transform[] spawnPoints; //guarda os pontos de spawns
 
 
     void Start ()
     {
-        InvokeRepeating ("Spawn", spawnTime, spawnTime);
+        InvokeRepeating ("Spawn", spawnTime, spawnTime); //chama repetitivamente o método Spawn
     }
 
-
+	//cria o inimigo em um determinado lugar da fase
     void Spawn ()
     {
+		//se o jogador morreu, não dar mais spawns
         if(playerHealth.currentHealth <= 0f)
         {
             return;
         }
 
-        int spawnPointIndex = Random.Range (0, spawnPoints.Length);
+        int spawnPointIndex = Random.Range (0, spawnPoints.Length); //pega aleatoriamente um dos pontos de spawn passados como parâmetro
 
+		//instancia o inimigo
         Instantiate (enemy, spawnPoints[spawnPointIndex].position, spawnPoints[spawnPointIndex].rotation);
     }
 }
