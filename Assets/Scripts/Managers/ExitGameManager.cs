64992c42954225d9c5f2a38d11c75794757599b2
Adriano Veríssimo <adrverissimo@yahoo.com.br>
2020-04-12 17:50:07 -0300

    Adicionando um painel para sair do jogo a partir da tela principal.

---
+++ Assets/Scripts/Managers/ExitGameManager.cs
@@ -0,0 +1,15 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEditor;
+
+public class ExitGameManager : MonoBehaviour
+{
+
+    public virtual void ExitGame()
+    {
+        if (!Application.isEditor)
+            Application.Quit();
+        else
+            EditorApplication.isPlaying = false;
+    }
+}
